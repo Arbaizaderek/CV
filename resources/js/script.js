07a9439155f9a31d3c59d99f548dfb0ef9afd640
Derek <arbaizaderek@hotmail.com>
2020-08-27 17:57:59 -0600

    Scripts staring to get added, clock added and a few alerts added

---
+++ resources/js/script.js
@@ -0,0 +1,39 @@
+var rightNow = new Date();
+document.getElementById("clock").innerHTML = rightNow.getMonth()+1+"/"+rightNow.getDate()+"/"+rightNow.getFullYear();
+
+function HTML(){
+    alert("HTML");
+};
+function CSS(){
+    alert("CSS");
+};
+function JavaScript(){
+    alert("JavaScript");
+};
+function Bootstrap(){
+    alert("HTML");
+};
+function Git(){
+    alert("Git");
+};
+function cSharp(){
+    alert("C#");
+};
+function Java(){
+    alert("Java");
+};
+function Python(){
+    alert("Python");
+};
+function SQL(){
+    alert("SQL");
+};
+function Office(){
+    alert("Office");
+};
+function Adobe(){
+    alert("Adobe");
+};
+function Google(){
+    alert("Google");
+};
